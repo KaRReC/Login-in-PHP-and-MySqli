5ad0da3ab846f5e5196aff2b81e35b1565727958
KaRReC <39775135+KaRReC@users.noreply.github.com>
2018-06-14 10:32:39 +0200

    navbar

---
+++ PHP/include/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+      <div class="container">
+        <a class="navbar-brand float-sm-right" href="index.php"><strong>Dashboard</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+          <ul class="navbar-nav mr-auto">
+          </ul>
+          <ul class="navbar-nav mr-left">
+            <li class="nav-item float-sm-right">
+              <a class="nav-link" href="SignUp.php">Sign up</a>
+            </li>
+            <li class="nav-item float-sm-right">
+              <a class="nav-link" href="Login.php">Log in<span class="sr-only"></span></a>
+            </li> 
+            <li class="nav-item float-sm-right">
+              <a class="nav-link" href="Logout.php">Log out</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
